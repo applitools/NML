717b3855fa1778f8c9529a4c02544aa10a8858cd
Shreya Sharma <shreyasocial@gmail.com>
2026-06-21 23:10:55 +0530

    Use AppiumRunner for NML classic runner

--- src/test/java/com/applitools/nml/BaseTest.java
+++ src/test/java/com/applitools/nml/BaseTest.java
@@ -1,8 +1,10 @@
 package com.applitools.nml;
 
 import com.applitools.eyes.BatchInfo;
-import com.applitools.eyes.config.Configuration;
+import com.applitools.eyes.EyesRunner;
+import com.applitools.eyes.appium.AppiumRunner;
 import com.applitools.eyes.appium.Eyes;
+import com.applitools.eyes.config.Configuration;
 import io.appium.java_client.android.AndroidDriver;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.openqa.selenium.remote.DesiredCapabilities;
@@ -16,14 +18,16 @@ public class BaseTest {
 
     protected static final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
-    protected static final String APPLITOOLS_API_KEY = env("APPLITOOLS_API_KEY");
-    protected static final String EYES_APP_NAME      = env("EYES_APP_NAME",   "NML Android Sample");
-    protected static final String EYES_BATCH_NAME    = env("EYES_BATCH_NAME", "NML Android Local Tests");
-    protected static final String APPIUM_URL         = env("APPIUM_URL",      "http://127.0.0.1:4723");
-    protected static final String AVD_NAME           = env("AVD_NAME",        "Pixel_8_API_35");
-    protected static final String PLATFORM_VERSION   = env("PLATFORM_VERSION","15.0");
+    protected static final String  APPLITOOLS_API_KEY   = env("APPLITOOLS_API_KEY");
+    protected static final String  EYES_APP_NAME        = env("EYES_APP_NAME",         "NML Android Sample");
+    protected static final String  EYES_BATCH_NAME      = env("EYES_BATCH_NAME",        "NML Android Local Tests");
+    protected static final String  APPIUM_URL           = env("APPIUM_URL",             "http://127.0.0.1:4723");
+    protected static final String  AVD_NAME             = env("AVD_NAME",               "Pixel_8_API_35");
+    protected static final String  PLATFORM_VERSION     = env("PLATFORM_VERSION",       "15.0");
+    protected static final boolean MULTI_DEVICE_ENABLED = Boolean.parseBoolean(env("NML_MULTI_DEVICE_ENABLED", "false"));
 
-    protected static BatchInfo batch;
+    protected static BatchInfo  batch;
+    protected static EyesRunner runner;
 
     protected static String env(String key) {
         String val = dotenv.get(key, null);
@@ -36,12 +40,15 @@ public class BaseTest {
     }
 
     @BeforeSuite
-    public void setUpBatch() {
-        batch = new BatchInfo(EYES_BATCH_NAME);
+    public void setUpSuite() {
+        batch  = new BatchInfo(EYES_BATCH_NAME);
+        runner = new AppiumRunner();
     }
 
     @AfterSuite
-    public void tearDownBatch() {}
+    public void tearDownSuite() {
+        if (runner != null) runner.getAllTestResults(false);
+    }
 
     protected AndroidDriver createDriver(String appPath, String appPackage, String appActivity) throws Exception {
         DesiredCapabilities caps = new DesiredCapabilities();
@@ -67,7 +74,7 @@ public class BaseTest {
         config.setBatch(batch);
         config.setSendDom(true);
 
-        Eyes eyes = new Eyes();
+        Eyes eyes = new Eyes(runner);
         eyes.setConfiguration(config);
         return eyes;
     }
