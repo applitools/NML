d685b45044fbdec9c6709371d5febf4fa94f008c
Shreya Sharma <shreyasocial@gmail.com>
2026-07-08 00:50:42 +0530

    Use ClassicRunner for Eyes instances in accessibility and analyticsX tests

--- test/specs/android/accessibility.android.test.js
+++ test/specs/android/accessibility.android.test.js
@@ -1,10 +1,12 @@
-import { Eyes, Target, BatchInfo, Configuration } from '@applitools/eyes-webdriverio';
+import { Eyes, ClassicRunner, Target, BatchInfo, Configuration } from '@applitools/eyes-webdriverio';
 
 describe('Accessibility Android NML - LambdaTest', () => {
-  let eyes;
+  let eyes, runner;
 
   before(async () => {
-    eyes = new Eyes();
+
+    runner = new ClassicRunner();
+    eyes = new Eyes(runner);
     eyes.setLogHandler({ type: 'file', filename: './logs/eyes_accessibility_lambdatest.log' });
     const config = new Configuration();
     config.setUseDom(true);
--- test/specs/android/analyticsX.android.test.js
+++ test/specs/android/analyticsX.android.test.js
@@ -1,12 +1,15 @@
-import { Eyes, Target, BatchInfo } from '@applitools/eyes-webdriverio';
+import { Eyes, ClassicRunner, Target, BatchInfo } from '@applitools/eyes-webdriverio';
 
 const FLOW = process.env.FLOW ?? 'full';
 
 describe('AnalyticsX Android NML - LambdaTest', () => {
-  let eyes;
+  let eyes,runner;
 
   before(async () => {
-    eyes = new Eyes();
+
+
+    runner = new ClassicRunner();
+    eyes = new Eyes(runner);
     eyes.setLogHandler({ type: 'file', filename: './logs/eyes_analyticsX_lambdatest.log' });
 
     eyes.setApiKey(process.env.APPLITOOLS_API_KEY);
