0d3b01b85c53b1a8d139e4871b765b59dc97122c
Shreya Sharma <shreyasocial@gmail.com>
2026-07-08 13:31:17 +0530

    Add TypeScript WDIO Appium NML iOS Multi-Device LambdaTest project
    Remove sample application binaries; keep this branch code-only.

---
+++ test/specs/ios/accessibility_nml_multidevice.ios.test.ts
@@ -0,0 +1,34 @@
+import { Eyes, Target, BatchInfo, Configuration } from '@applitools/eyes-webdriverio';
+
+describe('Accessibility iOS NML - LambdaTest', () => {
+  let eyes: Eyes;
+
+  before(async () => {
+    eyes = new Eyes();
+    eyes.setLogHandler({ type: 'file', filename: './logs/eyes_lambdatest.log' });
+    
+    const config = new Configuration();
+    config.addMultiDeviceTarget('iPhone 15 Pro Max', 'iPhone 11 Pro', 'iPhone 13',"iPhone 13 Pro Max");
+    config.setUseDom(true);
+    config.setSendDom(true);
+    eyes.setConfiguration(config);
+
+    eyes.setApiKey(process.env.APPLITOOLS_API_KEY as string);
+    eyes.setBatch(new BatchInfo('TS LambdaTest | Slicing Instrument | iOS Accessibility'));
+
+    await eyes.open(browser, 'LambdaTest iOS Accessibility App', 'iOS Accessibility Validation');
+    console.log('Eyes open');
+  });
+
+  after(async () => {
+    await eyes.abortIfNotClosed();
+  });
+
+  it('validates the Accessibility app main screen', async () => {
+    await eyes.check('Main Screen', Target.window());
+    console.log('Checked: Main Screen');
+
+    await eyes.close(false);
+    console.log('Eyes closed');
+  });
+});
