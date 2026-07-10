aaa36be72e65367c8cf6bbc25ce93dacb5015c7a
Shreya Sharma <shreyasocial@gmail.com>
2026-07-10 23:32:41 +0530

    Standardize batch name

--- test/specs/ios/accessibility.ios.test.js
+++ test/specs/ios/accessibility.ios.test.js
@@ -12,7 +12,7 @@ describe('Accessibility iOS NML - LambdaTest', () => {
     eyes.setConfiguration(config);
 
     eyes.setApiKey(process.env.APPLITOOLS_API_KEY);
-    eyes.setBatch(new BatchInfo('JS LambdaTest | Slicing Instrument | iOS Accessibility'));
+    eyes.setBatch(new BatchInfo('JS LambdaTest | Static/Slicing Dynamic | NML | iOS Accessibility'));
 
     await eyes.open(browser, 'LambdaTest iOS Accessibility App', 'iOS Accessibility Validation');
     console.log('Eyes open');
