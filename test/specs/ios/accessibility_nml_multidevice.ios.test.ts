fde3c90282e13a43a4d6522716da94d9bc882573
Shreya Sharma <shreyasocial@gmail.com>
2026-07-10 23:14:44 +0530

    Standardize batch name and simplify README

--- test/specs/ios/accessibility_nml_multidevice.ios.test.ts
+++ test/specs/ios/accessibility_nml_multidevice.ios.test.ts
@@ -17,7 +17,7 @@ describe('Accessibility iOS NML - BrowserStack', () => {
     eyes.setConfiguration(config);
 
     eyes.setApiKey(process.env.APPLITOOLS_API_KEY as string);
-    eyes.setBatch(new BatchInfo('TS BrowserStack | Static / Slicing Dynamic | iOS Accessibility'));
+    eyes.setBatch(new BatchInfo('TS BrowserStack | NML | iOS Accessibility | Multi Device'));
 
     await eyes.open(browser, 'BrowserStack iOS Accessibility App', 'iOS Accessibility Validation');
     console.log('Eyes open');
