5972519dc9de50e24228d533e2bbdd716ab39dbc
Shreya Sharma <shreyasocial@gmail.com>
2026-07-13 11:33:03 +0530

    Add local/javascript-wdio-appium-ios-multi-device-plug-and-play plug-and-play project

---
+++ test/specs/ios/accessibility_nml_multidevice.ios.test.js
@@ -0,0 +1,39 @@
+import { Eyes, ClassicRunner, Target, BatchInfo, Configuration } from '@applitools/eyes-webdriverio';
+
+describe('Accessibility iOS NML - Local', () => {
+  let eyes;
+  let runner;
+  let config;
+
+  before(async () => {
+    runner = new ClassicRunner();
+    eyes = new Eyes(runner);
+    eyes.setLogHandler({ type: 'file', filename: './logs/eyes_local.log' });
+
+    config = new Configuration();
+    config.setUseDom(true);
+    config.setSendDom(true);
+    config.addMultiDeviceTarget('iPhone 15 Pro Max', 'iPhone 11 Pro', 'iPhone 13');
+    eyes.setConfiguration(config);
+
+    eyes.setApiKey(process.env.APPLITOOLS_API_KEY);
+    eyes.setBatch(new BatchInfo('JS Local | Static / Slicing Dynamic | iOS Accessibility'));
+
+    await eyes.open(browser, 'Local iOS Accessibility App', 'iOS Accessibility Validation');
+    console.log('Eyes open');
+  });
+
+  after(async () => {
+    await eyes.abortIfNotClosed();
+  });
+
+  it('validates the Accessibility app main screen', async () => {
+    await eyes.check('Main Screen', Target.window());
+    console.log('Checked: Main Screen');
+
+    await eyes.check('Main Screen | Fully', Target.window().fully());
+
+    await eyes.close(false);
+    console.log('Eyes closed');
+  });
+});
