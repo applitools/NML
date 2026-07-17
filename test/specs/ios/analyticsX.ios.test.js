7c9b14ab7302f6d25f2829f1e155691499026bd0
Shreya Sharma <shreya.sharma@applitools.com>
2026-07-17 15:41:06 +0530

    Use ClassicRunner + eyes_local.log everywhere

--- test/specs/ios/analyticsX.ios.test.js
+++ test/specs/ios/analyticsX.ios.test.js
@@ -1,12 +1,13 @@
-import { Eyes, Target, BatchInfo } from '@applitools/eyes-webdriverio';
+import { Eyes, ClassicRunner, Target, BatchInfo } from '@applitools/eyes-webdriverio';
 
 const FLOW = process.env.FLOW ?? 'full';
 
 describe('AnalyticsX iOS NML - Local', () => {
-  let eyes;
+  let eyes, runner;
 
   before(async () => {
-    eyes = new Eyes();
+    runner = new ClassicRunner();
+    eyes = new Eyes(runner);
     eyes.setLogHandler({ type: 'file', filename: './logs/eyes_local.log' });
     eyes.setApiKey(process.env.APPLITOOLS_API_KEY);
     eyes.setBatch(new BatchInfo('JS Local | NML | iOS AnalyticsX'));
