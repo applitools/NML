22664d41bcba25fb582d67eb623a78d305efd477
Shreya Sharma <shreya.sharma@applitools.com>
2026-07-17 15:40:37 +0530

    Use ClassicRunner + eyes_lambdatest.log everywhere

--- test/specs/ios/accessibility.ios.test.js
+++ test/specs/ios/accessibility.ios.test.js
@@ -1,11 +1,12 @@
-import { Eyes, Target, BatchInfo, Configuration } from '@applitools/eyes-webdriverio';
+import { Eyes, ClassicRunner, Target, BatchInfo, Configuration } from '@applitools/eyes-webdriverio';
 
 describe('Accessibility iOS NML - LambdaTest', () => {
-  let eyes;
+  let eyes, runner;
 
   before(async () => {
-    eyes = new Eyes();
-   eyes.setLogHandler({ type: 'file', filename: './logs/eyes_lambdatest.log' });
+    runner = new ClassicRunner();
+    eyes = new Eyes(runner);
+    eyes.setLogHandler({ type: 'file', filename: './logs/eyes_lambdatest.log' });
      const config = new Configuration();
     config.setUseDom(true);
     config.setSendDom(true);
